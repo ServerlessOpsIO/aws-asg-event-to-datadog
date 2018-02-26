dd3c04a3945add94e0bbb677fb3e19ba5640d86f
Tom McLaughlin <tom@serverlessops.io>
2018-02-25 19:52:06 -0600

    Add region

--- handlers/write-event-to-datadog.py
+++ handlers/write-event-to-datadog.py
@@ -43,6 +43,7 @@ def _create_datadog_event(event: dict) -> dict:
     datadog_event['text'] = _get_event_text(
         asg=event.get('detail').get('AutoScalingGroupName'),
         host=event.get('detail').get('EC2InstanceId'),
+        region=event.get('region')
         detail=datadog_event['title']
     )
 
@@ -57,14 +58,15 @@ def _create_datadog_event(event: dict) -> dict:
     return datadog_event
 
 
-def _get_event_text(asg: str, host: str, detail: str) -> str:
+def _get_event_text(asg: str, host: str, region:str, detail: str) -> str:
     msg = '''
     ASG: {asg}
     Host: {host}
+    Region: {region}
 
     Detail: {detail}
     '''
-    return msg.format(asg, host, detail)
+    return msg.format(asg=asg, host=host, region=region detail=detail)
 
 
 def handler(event, context):
